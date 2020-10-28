7a1df0ba4865bb7416da93106702d30e87fafed9
Balaji Vadlamudi <bava72@gmail.com>
2020-10-28 14:50:25 -0700

    Final 10/28

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,11 @@
+//variables for displayImage() function
+let shakeNumber=0;
+let shakeRector="7%";
+let shakeDirection=getRandomInteger()%5;
+
 document.body.onload=addElement;
 document.getElementById('askme').focus();
+
 
 //Create a new div element and set initial img
 function addElement () { 
@@ -11,6 +17,7 @@ function addElement () {
     myDiv2.style.alignItems='center';
     let myImg=document.createElement('img');
     myImg.setAttribute('id', 'myImg');
+    myImg.setAttribute('class', 'shakeimage');
     myImg.setAttribute('src', 'MbImages/magic8ball_start.png');
     myImg.setAttribute('height', '700');
     myImg.setAttribute('width', '700');
@@ -20,30 +27,72 @@ function addElement () {
 
 function checkForEnterKey(element) {
     if (event.key === 'Enter') {
-        displayRandomImage();
+        ask();
     }
 }
 
-function displayRandomImage() {
+function ask() {
     //validate input
     let input = document.getElementById('askme').value;
-    if (document.getElementById('askme').value.length<3) {
+    if (document.getElementById('askme').value.length<5) {
         document.getElementById('ErrorMsg').innerHTML='Too short. Enter again...';
         document.querySelector('img').src='MbImages/magic8ball_extra.png';
     } else {
-        document.getElementById('ErrorMsg').innerHTML='';
-        //display new image
-        let myImg=document.querySelector('img');
-        myImg.src='MbImages/magic8ball_'+getRandomInteger()+'.png';        
-        //reset input
-        document.getElementById('askme').value='';
+        document.getElementById('ErrorMsg').innerHTML=null;
+        //shake image few times and display random image
+        displayImage();
     }
     document.getElementById('askme').focus();
+    document.getElementById('askme').select();
 }
 
 function getRandomInteger() {
     return (Math.floor(Math.random()*20)+1);
 }
+
+function displayImage() {
+    let myImg=document.getElementById('myImg');
+    myImg.src='MbImages/magic8ball_extra.png';
+    shakeNumber++;
+    myImg.style.left="0%";
+    myImg.style.top="0%";
+    //shake image
+    if(shakeNumber%10===0) {
+        //display new image
+        myImg.src='MbImages/magic8ball_'+getRandomInteger()+'.png';
+        shakeNumber=0;
+        return;        
+    }
+    if (shakeDirection==1){
+        myImg.style.top=parseInt(myImg.style.top)+shakeRector;
+        myImg.src='MbImages/magic8ball_'+getRandomInteger()+'.png';
+    } else if (shakeDirection==2){
+        myImg.style.left=parseInt(myImg.style.left)+shakeRector;
+        myImg.src='MbImages/magic8ball_'+getRandomInteger()+'.png';
+    } else if (shakeDirection==3){
+        myImg.style.top=parseInt(myImg.style.top)-shakeRector;
+        myImg.src='MbImages/magic8ball_'+getRandomInteger()+'.png';
+    } else{
+        myImg.style.left=parseInt(myImg.style.left)-shakeRector;
+        myImg.src='MbImages/magic8ball_'+getRandomInteger()+'.png';
+    }
+    shakeDirection<4 ? shakeDirection++ : shakeDirection=1;
+    setTimeout("displayImage()",50);
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
